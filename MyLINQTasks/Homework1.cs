e469a92d9967fe6697ffbfb150117782c25495e4
Yaroslav <Yaroslav.Rybak64@gmail.com>
2020-06-22 13:35:33 +0300

    add Task 51 and Homeworks from 2018 year Xd

---
+++ MyLINQTasks/Homework1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLINQTasks
+{
+    class Homework1
+    {
+        static public void Homework01()
+        {
+            string str = "Davis, Clyne, Fonte, Hooiveld, Shaw, Davis, Schneiderlin, Cork, Lallana, Rodriguez, Lambert";
+            var A = str.Split(new[] { ',', ' ' }).Where(x => x.Length > 0).Select((x, index) =>  (index + 1) + ". " + x + ", ").Aggregate((x,y) => x + y);
+        }
+        static public void Homework02()
+        {
+            string str = "Jason Puncheon, 26/06/1986; Jos Hooiveld, 22/04/1983; Kelvin Davis, 29/09/1976; Luke Shaw, 12/07/1995; Gaston Ramirez, 02/12/1990; Adam Lallana, 10/05/1988";
+            var A = str.Split(';').Select(x => x.Split(new[] { ',', ' ' })).OrderBy(x => DateTime.ParseExact(x.Last(), "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture))
+                .Select(x => x.Aggregate((y, z) => y + " " + z)).ToArray();
+        }
+        static public void Homework03()
+        {
+            string str = "4:12,2:43,3:51,4:29,3:24,3:14,4:46,3:25,4:52,3:27";
+            var A = new TimeSpan(0,0,(int)str.Split(',').Sum(x => (long)TimeSpan.Parse("00:0" + x).TotalSeconds));
+        }
+    }
+}
---
+++ MyLINQTasks/Task52.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLINQTasks
+{
+    class Task52
+    {
+        //        Даны строковые последовательности A и B; все
+        //строки в каждой последовательности различны, имеют ненулевую длину и содержат только цифры и заглавные буквы
+        //латинского алфавита.Получить последовательность всевозможных комбинаций вида «EA=EB», где EA — некоторый элемент из A, EB — некоторый элемент из B, причем оба элемента оканчиваются цифрой(например, «AF3= D78»). Упорядочить полученную последовательность в лексикографическом
+        //порядке по возрастанию элементов EA, а при одинаковых
+        //элементах EA — в лексикографическом порядке по убыванию
+        //элементов EB.
+        //Указание.Для перебора комбинаций использовать методы
+        //SelectMany и Select.
+        static public void Task()
+        {
+            Console.WriteLine("Task 52");
+            Random rand = new Random();
+            var A = Program.GetEnumerableStringWithLetters(50, rand);
+            var B = Program.GetEnumerableStringWithLetters(50, rand);
+            var C = A.Where(x => char.IsDigit(x.Last()) == true).Select(x => x).Join(B.Where(x => char.IsDigit(x.Last()) == true).Select(x => x), x => x, y => y, (x, y) => x + "=" + y).ToArray();
+
+        }
+    }
+}
